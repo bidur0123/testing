3ada21b222b379f21a7d64f7d0c48e65e43767d3
Bidur Gupta <bidurgupta125@gmail.com>
2023-10-08 23:57:11 +0530

    Added new widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widget_testing/Radial_Hero_Animation/radial_hero_animation.dart';
 import 'package:widget_testing/bike_ui/screens/home_screen.dart';
 import 'package:widget_testing/hero_text_animation/screens/home_screen.dart';
 import 'package:widget_testing/screen/custom_3d_cube.dart';
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePageUi(),
+      home: RadialExpansionDemo(),
     );
   }
 }
